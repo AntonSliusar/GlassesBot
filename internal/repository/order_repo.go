7e89aa9fb442a1e53cd5509ffcc14c1be9a8fca7
AntonSliusar <uk17091@gmail.com>
2025-10-18 15:44:11 +0300

    fix mistakes in order repo and order manager

--- internal/repository/order_repo.go
+++ internal/repository/order_repo.go
@@ -2,6 +2,7 @@ package repository
 
 import (
 	"database/sql"
+	"fmt"
 	"glassesbot/internal/domain"
 )
 
@@ -9,13 +10,16 @@ type OrderRepository struct {
 	db *sql.DB
 }
 
-func NewOrderRepositiry(db *sql.DB) *OrderRepository {
+func NewOrderRepository(db *sql.DB) *OrderRepository {
 	return &OrderRepository{db: db}
 }
 
 func (r *OrderRepository) Save (order *domain.Order) error {
-	_, err := r.db.Exec("INSERT INTO orders (frame, lensses, date, working_time) VALUES ($1, $2, $3, $4)",
-		order.Frame, order.Lenses, order.Date, order.WorkingTime)
+
+	res, err := r.db.Exec("INSERT INTO orders (frame, lenses, work_date, working_time) VALUES ($1, $2, $3, $4)",
+		order.Frame, order.Lenses, order.Date, int64(order.WorkingTime.Minutes()))
+
+	fmt.Println(res)
 
 	return err
 }
--- internal/service/order_manager.go
+++ internal/service/order_manager.go
@@ -27,7 +27,7 @@ func NewOrderManager(repo *repository.OrderRepository) *OrderManager {
 func (m *OrderManager) CreateOrder() int64 {
 	m.mutex.Lock()
 	defer m.mutex.Unlock()
-	randomID := int64(time.Now().Unix())
+	randomID := int64(time.Now().Unix() / 10)
 	
 	order := domain.NewOrder()
 	m.Orders[randomID] = order
@@ -46,7 +46,6 @@ func (m *OrderManager) PauseOrder(id int64) error {
 	if order.Status != domain.STATUS_IN_WORK {
 		return fmt.Errorf("Order with Id %d is not in work", id)
 	}
-
 	order.Pause()
 	return nil
 }
@@ -59,7 +58,7 @@ func (m *OrderManager) ResumeOrder(id int64) error {
 	if !exist {
 		return fmt.Errorf("Order with ID %d not found", id)
 	}
-	if order.Status != domain.STATUS_PAUSER {
+	if order.Status != domain.STATUS_PAUSE {
 		return fmt.Errorf("Order with ID %d is not paused", id)
 	}
 	order.Resume()
@@ -72,19 +71,21 @@ func (m *OrderManager) FinishOrder(id int64) error {
 
 	order, exist := m.Orders[id]
 	if !exist {
-		return fmt.Errorf("Order with ID %d not found", id)
+		fmt.Printf("Order with ID %d not found", id)
+		
 	}
 	order.TotalWorkinTime()
 
 	if err := m.repository.Save(order); err != nil {
-		return fmt.Errorf("Failde to save order: &w", err)
+		fmt.Printf("Failde to save order: &w", err)
+		return err
 	}
 	delete(m.Orders, id)
 	return nil
 }
 
 func (m *OrderManager) GetAllOrders() map[int64]*domain.Order {
-	m.mutex.RLock()
+	m.mutex.Lock()
 	defer m.mutex.Unlock()
 	
 	return m.Orders
