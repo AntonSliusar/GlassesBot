2d6f460e50b3cc08fc9f7c17a3a79866aceb4583
AntonSliusar <uk17091@gmail.com>
2025-10-18 15:45:06 +0300

    .

--- internal/db/postgres.go
+++ internal/db/postgres.go
@@ -8,15 +8,14 @@ import (
 
 func NewConnection(dbConfig *config.DBConfig) (*sql.DB, error) {
 	dsn := fmt.Sprintf(
-		"host=%s port=%s user=%s password=%s dbname=%s sslmode=%s",
-		dbConfig.Host,
-		dbConfig.Port,
-		dbConfig.User,
-		dbConfig.Password,
-		dbConfig.DBName,
-		dbConfig.SSLMode,
-	)
-
+        "host=%s port=%s user=%s password=%s dbname=%s sslmode=%s",
+        dbConfig.Host,
+        dbConfig.Port,
+        dbConfig.User,
+        dbConfig.Password,
+        dbConfig.DBName,
+        dbConfig.SSLMode,
+    )
 	db, err := sql.Open("postgres", dsn)
 	if err != nil {
 		return nil, err
