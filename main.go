428f92e18b1cd23b0008219f390273775c6a53fd
AntonSliusar <uk17091@gmail.com>
2025-10-18 15:49:50 +0300

    main file

---
+++ main.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"fmt"
+	"glassesbot/config"
+	"glassesbot/internal/db"
+	"glassesbot/internal/repository"
+	"glassesbot/internal/service"
+	"glassesbot/internal/telegram"
+	"log"
+	"os"
+
+	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
+)
+
+func main() {
+
+	//TODO: telegram bot functionality,  cover with tests, Docker file
+
+	config, err := config.LoadConfig()
+	if err != nil {
+		log.Fatalf("Error loading config: %v", err)
+	}
+
+	dbConn, err := db.NewConnection(config)
+	if err != nil {
+		log.Fatalf("Error connecting to database: %v", err)
+	}
+	defer dbConn.Close()
+
+
+	err = db.Migrate(dbConn)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+
+	orderRepo := repository.NewOrderRepository(dbConn)
+	orderManager := service.NewOrderManager(orderRepo)
+
+	
+	botAPI, err := tgbotapi.NewBotAPI(os.Getenv("TG_BOT_TOKEN"))
+	if err != nil {
+		log.Panic(err)
+	}
+
+	tgBot := telegram.NewBot(botAPI, orderManager)
+	tgBot.Start()
+
+}
