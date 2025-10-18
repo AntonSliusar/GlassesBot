b829ea756607ca6608a8443b5f5aec26eb68de9a
AntonSliusar <uk17091@gmail.com>
2025-10-18 15:45:48 +0300

    Create migrations for database and Docker compose file

---
+++ internal/db/migration.go
@@ -0,0 +1,30 @@
+package db
+
+import (
+	"database/sql"
+	"os"
+
+	"github.com/golang-migrate/migrate/v4"
+	"github.com/golang-migrate/migrate/v4/database/postgres"
+	_ "github.com/golang-migrate/migrate/v4/source/file"
+)
+
+func Migrate(db *sql.DB) error {
+	driver, err := postgres.WithInstance(db, &postgres.Config{})
+	if err != nil {
+		return err
+	}
+
+	m, err := migrate.NewWithDatabaseInstance(
+		os.Getenv("MIGRATION_PATH"),
+		"postgres", driver,
+	)
+	if err != nil {
+		return err
+	}
+
+	if err := m.Up(); err != nil && err != migrate.ErrNoChange {
+		return err
+	}
+	return nil	
+}
